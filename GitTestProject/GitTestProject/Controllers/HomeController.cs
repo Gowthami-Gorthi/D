f4f17e21c6f709b506243f3f6e9c0ba14f2b4474
Gowthami <gorthigowthami8@gmail.com>
2022-05-23 20:05:58 +0530

    Register method is created and some db files are added

--- GitTestProject/GitTestProject/Controllers/HomeController.cs
+++ GitTestProject/GitTestProject/Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GitTestProject.Models;
 
 namespace GitTestProject.Controllers
 {
     public class HomeController : Controller
     {
+        empManagementEntities con=new empManagementEntities();
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
@@ -16,15 +18,46 @@ namespace GitTestProject.Controllers
         }
         public ActionResult Login()
         {
-            ViewBag.Title = "Login Page";
-
+           return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string Login, string Password)
+        {
+            var res=(from t in con.employees where t.Login == Login &&t.Password == Password select t).FirstOrDefault();
+            if (res != null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Titl = "Invalid "+Login+" and "+Password;
+                return View();
+            }
+        }
+        #region Register
+        public ActionResult Register()
+        {
             return View();
         }
         [HttpPost]
-        public ActionResult Login(string uname, string pass)
+        public ActionResult Register(employee employee)
         {
-            ViewBag.Title = "valid";
-            return View(uname, pass);
+            if(ModelState.IsValid)
+            {
+                con.employees.Add(employee);
+                if (con.SaveChanges() > 0)
+                {
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    ViewData["msg"] = "invalid user";
+                    return View();
+                }
+            }
+            ViewData["msg"] = "invalid user";
+            return View();
         }
+            #endregion
     }
 }
